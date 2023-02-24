b4a6441e20a0a9d9143d5ceec9fe9b5ab04c0e33
jinny-codes <77732679+jinny-codes@users.noreply.github.com>
2023-02-25 00:25:19 +0900

    Added NoteForm

---
+++ src/main/java/happyforyou/foryou/controller/NoteForm.java
@@ -0,0 +1,16 @@
+package happyforyou.foryou.controller;
+
+import happyforyou.foryou.domain.Member;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+
+@Getter @Setter
+public class NoteForm {
+
+    @NotEmpty(message = "제목은 필수입니다")
+    private String title;
+    private String description;
+
+}
