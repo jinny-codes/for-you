58524bb474b3f5f518527a2472eab216fd969de6
jinny-codes <77732679+jinny-codes@users.noreply.github.com>
2023-02-26 00:41:18 +0900

    Added NoteDTO class

--- src/main/java/happyforyou/foryou/dto/NoteDto.java
+++ src/main/java/happyforyou/foryou/dto/NoteDto.java
@@ -1,2 +1,22 @@
-package happyforyou.foryou.dto;public class NoteDto {
+package happyforyou.foryou.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import happyforyou.foryou.domain.Comment;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@JsonInclude
+@Data
+public class NoteDto {
+
+    private Long id;
+    private String title;
+    private String description;
+    private List<Comment> comments;
+
 }
